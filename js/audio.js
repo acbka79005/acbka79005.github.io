ebef8d2a5dc75a7dcd8ae8ea7a5b076b6dc30e56
Serhii Vlasiuk <s.vlasiuk@ideil.com>
2019-01-28 21:21:57 +0200

    update list

--- js/audio.js
+++ js/audio.js
@@ -10,24 +10,29 @@ var AUDIO = function () {
             btn: '.js-player-btn'
         },
         Class = {
-            isActive: 'is-active'
+            isActive: 'is-active',
+            isError: 'is-error'
         },
         $player = document.querySelectorAll(Selector.player)[0],
         $btn = document.querySelectorAll(Selector.btn)[0];
 
-    if (!$player) {
-        return;
+    if (!$btn) {
+        return false;
     }
 
     $btn.addEventListener('click', function () {
-        if ($player.paused) {
-            $player.play();
+        if ($player) {
+            if ($player.paused) {
+                $player.play();
 
-            $btn.classList.add(Class.isActive);
+                $btn.classList.add(Class.isActive);
+            } else {
+                $player.pause();
+
+                $btn.classList.remove(Class.isActive);
+            }
         } else {
-            $player.pause();
-
-            $btn.classList.remove(Class.isActive);
+            $btn.classList.add(Class.isError);
         }
     });
 
